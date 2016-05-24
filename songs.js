8312250ccccbb505a8a4a08b4b671b954d52c099
Hannah Hall <hannahkhall13@gmail.com>
2016-05-24 15:59:37 -0500

    version 2 completed

---
+++ songs.js
@@ -0,0 +1,21 @@
+var songs = [];
+var input = document.getElementById("songList");
+
+songs[songs.length] = "Happy > by The Rolling Stones on the album Exile on Main St";
+songs[songs.length] = "Tumbling Dice > by The Rolling Stones on the album Exile on Main St";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "When I'm 64 > by The Beatles on the album Sgt. Peppers Lonely Hearts Club";
+songs[songs.length] = "With a Little Help for My Friends > by The Beatles on the album Sgt. Peppers Lonely Hearts Club";
+
+songs.push("Shoot Me In the Smile > by the Matches on the album Decomposer");
+songs.unshift("Gold on the Ceiling > by the Black Keys on the album El Camino");
+
+for (let i=0; i<songs.length; i++) {
+	songs[i] = songs[i].replace(">", "-");
+}
+
+for (let i=0; i<songs.length; i++) {
+	var inputInfo = `<p> ${songs[i]}</p>`;
+	input.innerHTML += inputInfo;
+}
+
